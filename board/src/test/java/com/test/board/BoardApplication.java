aa0bc94501dabb892220c954051931ec443a0a5a
nsns0101 <nsns0101@naver.com>
2021-10-30 04:10:23 +0900

    DB setting, Controller test

---
+++ board/src/test/java/com/test/board/BoardApplication.java
@@ -0,0 +1,12 @@
+package com.test.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+}
